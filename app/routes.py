4653c5922f0a27e22aabfe36421e538f3af86e06
Brendon Sayer <brendonsayer@Brendons-MacBook-Pro.local>
2022-06-13 17:41:08 -0700

    Added frequency flask form

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,15 @@
 from app import myapp_obj
-from flask import render_template
+from flask import render_template, request
 
 @myapp_obj.route("/")
 def index():
-    	return render_template('index.html')
+    return render_template('index.html')
+
+@myapp_obj.route("/process_signal", methods = ['POST'])
+def process_signal():
+	hz1 = request.form.get("hz1")
+	hz2 = request.form.get("hz2")
+
+	print(f'hz1:{hz1}hz2:{hz2}')
+
+	return render_template('index.html')
